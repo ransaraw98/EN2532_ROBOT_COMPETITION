8f80a55b40f72b026af5bd8f38a05787b65454eb
RVR <73053760+RathnamVR@users.noreply.github.com>
2021-07-08 20:30:56 +0530

    Delete robot directory

--- robot/controllers/Servo1_test/build/release/Servo1_test.d
+++
@@ -1,6 +0,0 @@
-build/release/Servo1_test.o: Servo1_test.cpp \
- G:\Webots/include/controller/cpp/webots/Motor.hpp \
- G:\Webots/include/controller/cpp/webots/Device.hpp \
- G:/Webots/include/controller/c/webots/types.h \
- G:\Webots/include/controller/cpp/webots/Robot.hpp \
- G:\Webots/include/controller/cpp/webots/Keyboard.hpp
