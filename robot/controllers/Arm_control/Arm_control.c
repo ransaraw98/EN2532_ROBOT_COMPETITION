5359a8b88594b1b708692e37c3ad55eb5594998c
Ransika <ransika1998@gmail.com>
2021-07-09 21:57:33 +0530

    Placing_box

--- robot/controllers/Arm_control/Arm_control.c
+++ robot/controllers/Arm_control/Arm_control.c
@@ -1,36 +1,60 @@
 #include <webots/distance_sensor.h>
 #include <webots/motor.h>
 #include <webots/robot.h>
+#include <stdio.h>
 
 #define TIME_STEP 64
 
-void Lifting_box(double linear,double S1,double S2,WbDeviceTag lr_1,WbDeviceTag lr_2,WbDeviceTag Sr_1,WbDeviceTag Sr_2){
+void Lifting_box(double *linear,double *S1,double *S2,WbDeviceTag lr_1,WbDeviceTag lr_2,WbDeviceTag Sr_1,WbDeviceTag Sr_2){
  
- while(linear>-0.02){
- linear -= 0.001;
- wb_motor_set_position(lr_1, -linear);
- wb_motor_set_position(lr_2, linear);
+ while(*linear>-0.02){
+ *linear -= 0.001;
+ wb_motor_set_position(lr_1, -*linear);
+ wb_motor_set_position(lr_2, *linear);
  wb_robot_step(TIME_STEP);
 }
 
-while(S1>-1){
- S1 -= 0.04;
- wb_motor_set_position(Sr_1, S1);
+while(*S1>-1){
+ *S1 -= 0.04;
+ wb_motor_set_position(Sr_1, *S1);
  wb_robot_step(TIME_STEP);
 }
-while(S2<=3.11){
- S2 += 0.04;
- wb_motor_set_position(Sr_2, S2);
- wb_robot_step(TIME_STEP);
-}
-
-while(S1>-2){
- S1 -= 0.04;
- wb_motor_set_position(Sr_1, S1);
+while(*S2<=3.11){
+ *S2 += 0.04;
+ wb_motor_set_position(Sr_2, *S2);
  wb_robot_step(TIME_STEP);
 }
 
+while(*S1>-2){
+ *S1 -= 0.04;
+ wb_motor_set_position(Sr_1, *S1);
+ wb_robot_step(TIME_STEP);
 }
+printf("Servor 1 value is %f\n", *S1);
+wb_robot_step(TIME_STEP);
+
+}
+
+
+void Placing_box(double *linear,double *S1,WbDeviceTag lr_1,WbDeviceTag lr_2,WbDeviceTag Sr_1){
+printf("Servor 1 value is %f\n", *S1);
+while(*S1<0){
+ *S1 += 0.04;
+ wb_motor_set_position(Sr_1, *S1);
+ wb_robot_step(TIME_STEP);
+}
+while(*linear<=0.0){
+ *linear += 0.001;
+ wb_motor_set_position(lr_1, -*linear);
+ wb_motor_set_position(lr_2, *linear);
+ wb_robot_step(TIME_STEP);
+}
+
+
+wb_robot_step(TIME_STEP);
+
+}
+
 
 
 int main() {
@@ -54,10 +78,12 @@ int main() {
   wb_motor_set_position(Sr_2, S2);
   wb_motor_set_position(Sr_1, S1);
   
-  Lifting_box(linear,S1,S2,lr_1,lr_2,Sr_1,Sr_2);
+  Lifting_box(&linear,&S1,&S2,lr_1,lr_2,Sr_1,Sr_2);
+  printf("Servor 1 value is %f\n", S1);
+  Placing_box(&linear,&S1,lr_1,lr_2,Sr_1);
   while (wb_robot_step(TIME_STEP) != -1) {
 
-
+  
   }
   wb_robot_cleanup();
   return 0;  // EXIT_SUCCESS
