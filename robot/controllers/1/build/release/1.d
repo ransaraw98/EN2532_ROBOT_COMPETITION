186d025bbefb37c533ddbb256f71f269beb0b8ff
Ransara <ransarawijitha@gmail.com>
2021-07-10 14:10:54 +0530

    Tested arm before arm solidifying commit
    it works, problem is with the arm solidifying commit

---
+++ robot/controllers/1/build/release/1.d
@@ -0,0 +1,6 @@
+build/release/1.o: 1.c \
+ C:\Users\menuw\AppData\Local\Programs\Webots/include/controller/c/webots/distance_sensor.h \
+ C:\Users\menuw\AppData\Local\Programs\Webots/include/controller/c/webots/types.h \
+ C:\Users\menuw\AppData\Local\Programs\Webots/include/controller/c/webots/motor.h \
+ C:\Users\menuw\AppData\Local\Programs\Webots/include/controller/c/webots/robot.h \
+ C:\Users\menuw\AppData\Local\Programs\Webots/include/controller/c/webots/nodes.h
