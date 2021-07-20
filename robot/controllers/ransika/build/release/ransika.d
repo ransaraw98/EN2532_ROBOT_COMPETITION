d4d031a334119f13eebc93c8f89eb5abb7a3db00
Ransara <ransarawijitha@gmail.com>
2021-07-20 13:59:13 +0530

    Added arm
    Success

---
+++ robot/controllers/ransika/build/release/ransika.d
@@ -0,0 +1,6 @@
+build/release/ransika.o: ransika.c \
+ C:\Users\menuw\AppData\Local\Programs\Webots/include/controller/c/webots/distance_sensor.h \
+ C:\Users\menuw\AppData\Local\Programs\Webots/include/controller/c/webots/types.h \
+ C:\Users\menuw\AppData\Local\Programs\Webots/include/controller/c/webots/motor.h \
+ C:\Users\menuw\AppData\Local\Programs\Webots/include/controller/c/webots/robot.h \
+ C:\Users\menuw\AppData\Local\Programs\Webots/include/controller/c/webots/nodes.h
