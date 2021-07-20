655966e489c9db89e70ee30ff135f1cc578b4377
Ransara <ransarawijitha@gmail.com>
2021-07-20 13:48:19 +0530

    Changed PID algorithm
    still fails to take the curved 90 degree turn
    apparently the motors cannot be rotated in reverse?

--- robot/controllers/fF/fF.c
+++ robot/controllers/fF/fF.c
@@ -22,25 +22,27 @@
 /* Line Follow */
 #define nQTR 8
 #define MAX_GS 880 //840
-#define MIN_GS 300
+#define MIN_GS 40
 #define NEW_GS 1000 
-
+#define BUFLEN 7
+#define MID 3
 ////////////////////////////////////// *VARIABLES ///////////////////////////////////////////////
 /* QTR SENSOR ARRAY + LINE FOLLOW INITIALIZATIONS*/
 double qtrValues[8] = { 0,0,0,0,0,0,0,0 };
 double qtrNew[nQTR] = { 0, 0, 0, 0, 0,0,0,0 };
 double maxGS[nQTR] = { 500, 500, 500, 500, 500, 500, 500, 500 };
 double minGS[nQTR] = { 500, 500, 500, 500, 500, 500, 500, 500 };
+double qtrStore[7][5];
 char QTR_names[nQTR][6] = { "qtr0","qtr1","qtr2","qtr3","qtr4","qtr5","qtr6","qtr7" };
 char wheels_names[2][12] = { "right_motor", "left_motor" };
 double error[8] = { 0,0,0,0,0,0,0,0 };
-double weights[8] = { 0,3,1.1,0.6,0,-0.6,-1.1,-3 };
-double Kp = 0.15;
-double Kd = 0.25;
+double weights[8] = { 0,5,1.3,1,0,-1,-1.3,-5};
+double Kp = 0.45;
+double Kd = 0.1;
 double P = 0;
 double D = 0;
 double pEr = 0;
-double baseSpeed = 400;  //  rad/s
+double baseSpeed = 200;  //  rad/s
 double Speeds[2] = { 0,0 };
 double PID = 0;
 short hardLeft, hardRight, Tjunc = 0;
@@ -68,7 +70,39 @@ void readQTR(WbDeviceTag *QTRarray) {
 /* Function PROTOTYPES */
 void ReadQTR(WbDeviceTag* QTRa);
 void lineFollow(void);
+void swap(double* xp, double* yp);
+void selectionSort(double arr[], int n);
+void ReadQTR2(WbDeviceTag QTRa[]);
 //void hardLeftf(void);
+
+
+void swap(double* xp, double* yp)
+{
+    double temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+// Function to perform Selection Sort
+void selectionSort(double arr[], int n)
+{
+    int i, j, min_idx;
+
+    // One by one move boundary of unsorted subarray
+    for (i = 0; i < n - 1; i++) {
+
+        // Find the minimum element in unsorted array
+        min_idx = i;
+        for (j = i + 1; j < n; j++)
+            if (arr[j] < arr[min_idx])
+                min_idx = j;
+
+        // Swap the found minimum element
+        // with the first element
+        swap(&arr[min_idx], &arr[i]);
+    }
+}
+
 
 
 void ReadQTR(WbDeviceTag *QTRa) {
@@ -85,18 +119,20 @@ void ReadQTR(WbDeviceTag *QTRa) {
         if (qtrValues[i] > maxGS[i]) maxGS[i] = qtrValues[i];
 
         // linear Interpolation
-        qtrNew[i] = ((double)qtrValues[i] - MIN_GS) / (MAX_GS - MIN_GS) * NEW_GS;
+        qtrNew[i] = ((double)qtrValues[i] - minGS[i]) / (maxGS[i] - MIN_GS) * NEW_GS;
 
         // Limited values between 0 and 1000 (NEW_GS)
         if (qtrNew[i] > NEW_GS) qtrNew[i] = NEW_GS;
         if (qtrNew[i] < 0) qtrNew[i] = 0;
 
-        if (qtrNew[i] < 70) {
+        /*if (qtrNew[i] < 70) {
             error[i] = 0;
         }
         else {
             error[i] = 1000;//qtrNew[i] - 70;
         }
+        */
+        error[i] = qtrNew[i] - 70;
     }
     
      if ((error[1] == 0) && (error[2] == 0) && (error[3] == 0) && (error[4] == 0) && (error[5] == 0) && (error[6] == 1000) && (error[7] == 1000))  {
@@ -119,12 +155,48 @@ void ReadQTR(WbDeviceTag *QTRa) {
 
 }
 
+
+void ReadQTR2(WbDeviceTag QTRa[]) {
+
+    for (int i = 0; i < nQTR-1;i++) {
+
+        qtrStore[0][i] = wb_distance_sensor_get_value(QTRa[1]); // neglect leftmost sensor
+        qtrStore[1][i] = wb_distance_sensor_get_value(QTRa[2]);
+        qtrStore[2][i] = wb_distance_sensor_get_value(QTRa[3]);
+        qtrStore[3][i] = wb_distance_sensor_get_value(QTRa[4]);
+        qtrStore[4][i] = wb_distance_sensor_get_value(QTRa[5]);
+        qtrStore[5][i] = wb_distance_sensor_get_value(QTRa[6]);
+        qtrStore[6][i] = wb_distance_sensor_get_value(QTRa[7]);
+    
+    }
+    selectionSort(qtrStore[0], BUFLEN);
+    selectionSort(qtrStore[1], BUFLEN);
+    selectionSort(qtrStore[2], BUFLEN);
+    selectionSort(qtrStore[3], BUFLEN);
+    selectionSort(qtrStore[4], BUFLEN);
+    selectionSort(qtrStore[5], BUFLEN);
+    selectionSort(qtrStore[6], BUFLEN);
+
+    for (int i = 0; i < 7;i++) {
+        error[i] = qtrStore[i][3]-250;
+   
+    }
+    if ((error[0] < 350) && (error[1] < 350) && (error[2] < 350) && (error[3] < 350) && (error[4] < 350) && (error[5] > 400) && (error[6] > 400)) {
+
+        hardLeft = 1;
+        printf("hardleft");
+    }
+    else {
+        hardLeft = 0;
+    }
+
+}
 void hardLeftf(void) {
     const double initAngle = wb_inertial_unit_get_roll_pitch_yaw(IMU)[2];
     printf("HARDLEFT\n");
     while (wb_inertial_unit_get_roll_pitch_yaw(IMU)[2]-initAngle< 1.57) {
-        wb_motor_set_velocity(wheels[0], 1);
-        wb_motor_set_velocity(wheels[1], 0);
+        wb_motor_set_velocity(wheels[0], 3);
+        wb_motor_set_velocity(wheels[1], -0.25);
         wb_robot_step(TIME_STEP);
     }
     hardLeft = 0;
@@ -135,7 +207,7 @@ void hardRightf(void) {
     printf("HARDRIGHT\n");
     while (wb_inertial_unit_get_roll_pitch_yaw(IMU)[2]-initAngle > -1.57) {
         wb_motor_set_velocity(wheels[0], 0);
-        wb_motor_set_velocity(wheels[1], 1);
+        wb_motor_set_velocity(wheels[1], 3);
         wb_robot_step(TIME_STEP);
     }
     hardLeft = 0;
@@ -158,6 +230,20 @@ void lineFollow(void) {
     pEr = eSUM;
 }
 
+void lineFollow2(void) {
+    double eSUM2 = 0;
+    for (int i = 0; i < 7; i++) { //neglect the leftmost sensor
+        eSUM2 += error[i] * weights[i+1];
+    }
+    P = Kp * eSUM2;
+    D = Kd * (eSUM2 - pEr);
+    PID = P + D;
+    Speeds[0] = baseSpeed - PID;
+    Speeds[1] = baseSpeed + PID;
+    pEr = eSUM2;
+    printf("%f\n",PID);
+
+}
 
 unsigned char readColor(WbDeviceTag camera) {
     /*
@@ -249,17 +335,20 @@ int main(int argc, char** argv) {
          */
 
          //readQTR(QTR);
-        ReadQTR(QTR);
-        lineFollow();
+        ReadQTR2(QTR);
+        lineFollow2();
         if (hardLeft == 1) {
             hardLeftf();
         }
         if (hardRight == 1) {
             hardRightf();
         }
+        wb_motor_set_position(wheels[0], INFINITY);
         wb_motor_set_velocity(wheels[0], 0.00628 * Speeds[0]);
+        wb_motor_set_position(wheels[1], INFINITY);
         wb_motor_set_velocity(wheels[1], 0.00628 * Speeds[1]);
-        printf("%4f   %4f   %4f   %4f    %4f    %4f   %4f    %4f", qtrNew[0], qtrNew[1], qtrNew[2], qtrNew[3], qtrNew[4], qtrNew[5], qtrNew[6], qtrNew[7]);
+        //printf("%4f   %4f   %4f   %4f    %4f    %4f   %4f    %4f", qtrNew[0], qtrNew[1], qtrNew[2], qtrNew[3], qtrNew[4], qtrNew[5], qtrNew[6], qtrNew[7]);
+        printf("%4f   %4f   %4f   %4f    %4f    %4f   %4f    %4f", error[0], error[1], error[2], error[3], error[4], error[5], error[6], error[7]);
         /* Process sensor data here */
        
         printf("\t CAM1  %c \t CAM2  %c\n",readColor(CAM1),readColor(CAM2));
