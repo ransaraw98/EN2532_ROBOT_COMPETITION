867beb956d61a25e652589cb5a52f4f9ba218537
Ransara <ransarawijitha@gmail.com>
2021-07-29 21:06:04 +0530

    Circle complete with new arena

---
+++ robot/worlds/EN2532_Arena_v4_1/controllers/gate2/gate2.py
@@ -0,0 +1,29 @@
+from controller import Robot
+
+robot = Robot()
+timestep = 100
+
+m = robot.getDevice("motor2")
+m.setPosition(float('inf'))
+m.setVelocity(0.0)
+
+pSensor = robot.getPositionSensor("ps2")
+pSensor.enable(timestep)
+
+speed =0
+k = 0
+t=0
+while (robot.step(timestep) != -1):
+    m.setVelocity(speed)
+    k = pSensor.getValue()
+    t += timestep
+    if (k < 21.5) & (speed ==-0) & (t>3000):
+        speed =3
+    if (k > 22) & (speed ==3) & (t >3000) :
+        speed = 0
+    if (k > 22) & (speed == 0) & (t >13000):
+        speed = -3
+    if (k < 21.5) & (speed ==-3) & (t >13000):
+        speed = 0
+        t=-7000 
+    pass
