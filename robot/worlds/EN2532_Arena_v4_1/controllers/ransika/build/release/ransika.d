64ebc300b82f938b9234b956d7f9ecba9175ad14
Ransika <ransika1998@gmail.com>
2021-07-31 13:04:17 +0530

    Teeth changed

---
+++ robot/worlds/EN2532_Arena_v4_1/controllers/ransika/build/release/ransika.d
@@ -0,0 +1,6 @@
+build/release/ransika.o: ransika.c \
+ G:\Webots/include/controller/c/webots/distance_sensor.h \
+ G:\Webots/include/controller/c/webots/types.h \
+ G:\Webots/include/controller/c/webots/motor.h \
+ G:\Webots/include/controller/c/webots/robot.h \
+ G:\Webots/include/controller/c/webots/nodes.h
